fd7e80d8abcb2a46d9d696b5e63004fe95d19a2c
Julien Church <julienchurch@gmail.com>
2016-08-02 20:08:11 -0700

    Update scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s $(pwd)/vimrc ~/.vimrc
+vim +PlugInstall
